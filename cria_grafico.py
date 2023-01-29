6bffc2241073fdbde8a3749fbfd3f0995ee2ba3b
Paulo Viana <pauloviana089@gmail.com>
2023-01-29 20:49:03 -0300

    Add files via upload

---
+++ cria_grafico.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([[160],[165],[171],[174],[179],[181],[185],[188],[191],[200]])
+
+y = np.array([[64],[67],[70],[80],[77],[81],[87],[94],[101],[112]])
+
+print(plt.scatter(x, y))
